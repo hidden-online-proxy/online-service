9ab79d36fa3fddc3921689db764e078f745cdd70
hidden-github <hidden.github@gmail.com>
2024-08-14 21:58:39 -0400

    home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -14,7 +14,7 @@ if (
   const popup = open("about:blank", "_blank");
   if (!popup || popup.closed) {
     alert(
-  "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page."
+  "By using Canvas Timeline® services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the account page."
     );
 
   } else {
@@ -23,7 +23,7 @@ if (
     const style = iframe.style;
     const link = doc.createElement("link");
 
-    const name = localStorage.getItem("name") || "My Drive - Google Drive";
+    const name = localStorage.getItem("name") || "Dashboard";
     const icon =
       localStorage.getItem("icon") ||
       "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png";
@@ -173,16 +173,7 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "efficiently manage your assingments!",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
@@ -199,19 +190,9 @@ SplashE.addEventListener("click", US);
 // Random URL
 function getRandomUrl() {
   const randomUrls = [
-    "https://kahoot.it",
-    "https://classroom.google.com",
-    "https://drive.google.com",
-    "https://google.com",
-    "https://docs.google.com",
-    "https://slides.google.com",
-    "https://www.nasa.gov",
-    "https://blooket.com",
-    "https://clever.com",
-    "https://edpuzzle.com",
-    "https://khanacademy.org",
-    "https://wikipedia.org",
-    "https://dictionary.com",
+    "https://www.desmos.com/testing/virginia/graphing",
+    "https://va-roan-psv.edupoint.com/",
+    "https://launchpad.classlink.com/rcs/",
   ];
   return randomUrls[randRange(0, randomUrls.length)];
 }
